3a1999d905e3714327ef6039f03ce22698320c98
Adam H. Sparks <adamhsparks@gmail.com>
2017-11-10 06:45:34 +1000

    Check if Verdana has been imported
    Check if Verdana has been imported, if so don't do anything. If not import. Check again. If istill no, try a recursive import (e.g., Linux that puts MS fonts in a sub-directory).

--- R/zzz.R
+++ R/zzz.R
@@ -1,34 +1,42 @@
 
-
 .onLoad <-
   function(libname, pkgname) {
     pdfFonts <- grDevices::pdfFonts
     postscriptFonts <- grDevices::postscriptFonts
 
-    extrafont::loadfonts(device = "pdf", quiet = TRUE)
-    extrafont::loadfonts(device = "postscript", quiet = TRUE)
-
     if (.Platform$OS.type == "windows") {
-      # check for existence of Verdana.afm file before importing to avoid message
+      # check for Verdana.afm file before importing to avoid message
       # import as necessary
       if (!file.exists(system.file("metrics",
                                    "Verdana.afm.gz",
                                    package = "extrafontdb"))) {
         extrafont::font_import(prompt = FALSE,
                                recursive = FALSE)
+      } #if Verdana still not found, do a recursive import
+      if (!file.exists(system.file("metrics",
+                                   "Verdana.afm.gz",
+                                   package = "extrafontdb"))) {
+        extrafont::font_import(prompt = FALSE,
+                               recursive = TRUE)
       }
       windowsFonts <- grDevices::windowsFonts
       extrafont::loadfonts(device = "pdf", quiet = TRUE)
       extrafont::loadfonts(device = "postscript", quiet = TRUE)
       extrafont::loadfonts(device = "win", quiet = TRUE)
     } else if (Sys.info()["sysname"] == "Darwin") {
-      # check for existence of Verdana.afm file before importing to avoid message
+      # check for Verdana.afm file before importing to avoid message
       # import as necessary
       if (!file.exists(system.file("metrics",
                                    "Verdana.afm.gz",
                                    package = "extrafontdb"))) {
         extrafont::font_import(prompt = FALSE,
                                recursive = FALSE)
+      }  #if Verdana still not found, do a recursive import
+      if (!file.exists(system.file("metrics",
+                                   "Verdana.afm.gz",
+                                   package = "extrafontdb"))) {
+        extrafont::font_import(prompt = FALSE,
+                               recursive = TRUE)
       }
       extrafont::loadfonts(device = "pdf", quiet = TRUE)
       extrafont::loadfonts(device = "postscript", quiet = TRUE)
@@ -39,8 +47,14 @@
         "Verdana Bold Italic"
       ))
     } else {
-      # check for existence of Verdana.afm file before importing to avoid message
+      # check for Verdana.afm file before importing to avoid message
       # import as necessary
+      if (!file.exists(system.file("metrics",
+                                   "Verdana.afm.gz",
+                                   package = "extrafontdb"))) {
+        extrafont::font_import(prompt = FALSE,
+                               recursive = FALSE)
+      }  #if Verdana still not found, do a recursive import
       if (!file.exists(system.file("metrics",
                                    "Verdana.afm.gz",
                                    package = "extrafontdb"))) {
@@ -50,5 +64,4 @@
       extrafont::loadfonts(device = "pdf", quiet = TRUE)
       extrafont::loadfonts(device = "postscript", quiet = TRUE)
     }
-
   }
