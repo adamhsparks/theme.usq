d1d1316bb322927d54984a4d0ea7c8f17e0725a7
Adam H. Sparks <adamhsparks@gmail.com>
2017-11-09 18:21:05 +1000

    Update with new repository name

--- R/theme_usq-package.R
+++ R/theme_usq-package.R
@@ -8,8 +8,8 @@
 #' @seealso
 #'\strong{Useful links:}
 #' \itemize{
-#' \item{Development \url{https://github.com/adamhsparks/theme_usq}}
-#' \item{Report bugs at \url{https://github.com/adamhsparks/theme_usq/issues}}
+#' \item{Development \url{https://github.com/adamhsparks/theme.usq}}
+#' \item{Report bugs at \url{https://github.com/adamhsparks/theme.usq/issues}}
 #' \item{USQ guidelines (must be logged in)
 #'  \url{https://www.usq.edu.au/a-brand-hidden/guidelines}}
 #' }
