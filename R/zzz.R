f91470fd7a23aaf58a7070dd81d04cbe0b958ba3
Adam H. Sparks <adamhsparks@gmail.com>
2017-11-09 22:10:03 +1000

    Try again to capture linux ttf fonts

--- R/zzz.R
+++ R/zzz.R
@@ -2,15 +2,6 @@
 
 .onLoad <-
   function(libname, pkgname) {
-    # check for existence of Verdana.afm file before importing to avoid message
-    # import as necessary
-    if (!file.exists(system.file("metrics",
-                                 "Verdana.afm.gz",
-                                 package = "extrafontdb"))) {
-      extrafont::font_import(prompt = FALSE,
-                             recursive = FALSE)
-    }
-
     pdfFonts <- grDevices::pdfFonts
     postscriptFonts <- grDevices::postscriptFonts
 
@@ -18,9 +9,29 @@
     extrafont::loadfonts(device = "postscript", quiet = TRUE)
 
     if (.Platform$OS.type == "windows") {
+      # check for existence of Verdana.afm file before importing to avoid message
+      # import as necessary
+      if (!file.exists(system.file("metrics",
+                                   "Verdana.afm.gz",
+                                   package = "extrafontdb"))) {
+        extrafont::font_import(prompt = FALSE,
+                               recursive = FALSE)
+      }
       windowsFonts <- grDevices::windowsFonts
+      extrafont::loadfonts(device = "pdf", quiet = TRUE)
+      extrafont::loadfonts(device = "postscript", quiet = TRUE)
       extrafont::loadfonts(device = "win", quiet = TRUE)
     } else if (Sys.info()["sysname"] == "Darwin") {
+      # check for existence of Verdana.afm file before importing to avoid message
+      # import as necessary
+      if (!file.exists(system.file("metrics",
+                                   "Verdana.afm.gz",
+                                   package = "extrafontdb"))) {
+        extrafont::font_import(prompt = FALSE,
+                               recursive = FALSE)
+      }
+      extrafont::loadfonts(device = "pdf", quiet = TRUE)
+      extrafont::loadfonts(device = "postscript", quiet = TRUE)
       grDevices::quartzFonts(Verdana = c(
         "Verdana",
         "Verdana Bold",
@@ -28,6 +39,16 @@
         "Verdana Bold Italic"
       ))
     } else {
+      # check for existence of Verdana.afm file before importing to avoid message
+      # import as necessary
+      if (!file.exists(system.file("metrics",
+                                   "Verdana.afm.gz",
+                                   package = "extrafontdb"))) {
+        extrafont::font_import(prompt = FALSE,
+                               recursive = TRUE)
+      }
+      extrafont::loadfonts(device = "pdf", quiet = TRUE)
+      extrafont::loadfonts(device = "postscript", quiet = TRUE)
       grDevices::X11Fonts(Verdana = c(
         "Verdana",
         "Verdana Bold",
